5a6b154986859d31f9a9bd34e6264f8533b71f0c
Dave Brannigan <brannigd@hotmail.com>
2025-08-03 13:34:53 -0400

    fix: remove onSuccess from account query

--- frontend/src/evennia_replacements/queries.tsx
+++ frontend/src/evennia_replacements/queries.tsx
@@ -2,6 +2,7 @@ import { useQuery, useMutation } from '@tanstack/react-query'
 import { fetchHomeStats, fetchAccount, postLogin, postLogout } from './api'
 import { useAppDispatch } from '../store/hooks'
 import { setAccount } from '../store/authSlice'
+import { useEffect } from 'react'
 
 export function useHomeStats() {
   return useQuery({ queryKey: ['homepage'], queryFn: fetchHomeStats })
@@ -9,13 +10,18 @@ export function useHomeStats() {
 
 export function useAccountQuery() {
   const dispatch = useAppDispatch()
-  return useQuery({
+  const result = useQuery({
     queryKey: ['account'],
     queryFn: fetchAccount,
-    onSuccess: (data) => {
-      dispatch(setAccount(data))
-    },
   })
+
+  useEffect(() => {
+    if (result.data !== undefined) {
+      dispatch(setAccount(result.data))
+    }
+  }, [result.data, dispatch])
+
+  return result
 }
 
 export function useLogin(onSuccess?: () => void) {
