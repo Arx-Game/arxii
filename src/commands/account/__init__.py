88bfef7c39cd301cf0dee3d7425b18c8325ca2b6
Dave Brannigan <brannigd@hotmail.com>
2025-08-04 12:41:36 -0400

    Merge account commands into default cmdset (#61)

--- src/commands/account/__init__.py
+++ src/commands/account/__init__.py
@@ -5,6 +5,7 @@ These commands handle account management, character switching, and OOC functiona
 They should NOT use flows - account management is OOC and uses standard Django patterns.
 """
 
-from commands.account.cmdset import AccountCmdSet
+from commands.account.account_info import CmdAccount
+from commands.account.character_switching import CmdCharacters, CmdIC
 
-__all__ = ["AccountCmdSet"]
+__all__ = ["CmdIC", "CmdCharacters", "CmdAccount"]
--- src/commands/default_cmdsets.py
+++ src/commands/default_cmdsets.py
@@ -16,6 +16,9 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 
 from evennia import default_cmds
 
+from commands.account.account_info import CmdAccount
+from commands.account.character_switching import CmdCharacters, CmdIC
+
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
@@ -84,9 +87,12 @@ class AccountCmdSet(default_cmds.AccountCmdSet):
         Populates the cmdset
         """
         super().at_cmdset_creation()
-        #
-        # any commands you add below will overload the default ones.
-        #
+        for cmdname in ("ic", "characters", "account"):
+            self.remove(cmdname)
+
+        self.add(CmdIC())
+        self.add(CmdCharacters())
+        self.add(CmdAccount())
 
 
 class UnloggedinCmdSet(default_cmds.UnloggedinCmdSet):
