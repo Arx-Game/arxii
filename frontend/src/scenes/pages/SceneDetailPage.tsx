43382a1d42098da84ed6756c9f3f692e93c99e23
Dave Brannigan <brannigd@hotmail.com>
2025-08-13 15:49:11 -0400

    feat(frontend): refresh active scene (#130)

--- frontend/src/scenes/pages/SceneDetailPage.tsx
+++ frontend/src/scenes/pages/SceneDetailPage.tsx
@@ -6,13 +6,14 @@ import { SceneMessages } from '../components/SceneMessages';
 
 export function SceneDetailPage() {
   const { id = '' } = useParams();
-  const { data: scene } = useQuery<SceneDetail>({
+  const { data: scene, refetch } = useQuery<SceneDetail>({
     queryKey: ['scene', id],
     queryFn: () => fetchScene(id),
+    refetchInterval: (query) => (query.state.data?.is_active ? 60000 : false),
   });
   return (
     <div className="container mx-auto p-4">
-      <SceneHeader scene={scene} />
+      <SceneHeader scene={scene} onRefresh={() => refetch()} />
       <SceneMessages sceneId={id} />
     </div>
   );
