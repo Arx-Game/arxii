fc4b79f51d457d1844cd14535660a2e91c0d01a5
Dave Brannigan <brannigd@hotmail.com>
2025-08-15 23:25:54 -0400

    feat: add register link and auth prompts (#150)

---
+++ frontend/src/game/GamePage.test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react';
+import { GamePage } from './GamePage';
+import { renderWithProviders } from '../test/utils/renderWithProviders';
+import { store } from '../store/store';
+import { setAccount } from '../store/authSlice';
+import { mockAccount } from '../test/mocks/account';
+
+describe('GamePage', () => {
+  beforeEach(() => {
+    store.dispatch(setAccount(null));
+  });
+
+  it('prompts to log in when not authenticated', () => {
+    renderWithProviders(<GamePage />);
+    expect(screen.getByText(/you must be logged in/i)).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /log in/i })).toHaveAttribute('href', '/login');
+    expect(screen.getByRole('link', { name: /register/i })).toHaveAttribute('href', '/register');
+  });
+
+  it('shows game interface when authenticated', () => {
+    store.dispatch(setAccount(mockAccount));
+    renderWithProviders(<GamePage />);
+    expect(screen.queryByText(/you must be logged in/i)).not.toBeInTheDocument();
+  });
+});
