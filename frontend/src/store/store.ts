1be6d2fed13a9381da34a92438ef71cfe78c1a89
Arx Developer <surly.mime@gmail.com>
2026-05-30 00:23:23 +0000

    feat(combat): deepLinkModal redux slice (#551)

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { gameSlice } from './gameSlice';
 import { authSlice } from './authSlice';
 import { rouletteSlice } from './rouletteSlice';
+import { deepLinkModalSlice } from './deepLinkModalSlice';
 
 export const store = configureStore({
   reducer: {
     game: gameSlice.reducer,
     auth: authSlice.reducer,
     roulette: rouletteSlice.reducer,
+    deepLinkModal: deepLinkModalSlice.reducer,
   },
 });
 
