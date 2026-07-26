7379efec4977433eadd279fe6c34fda1a971a283
Arx Developer <surly.mime@gmail.com>
2026-07-26 14:52:55 +0000

    fix: regenerate API types from fresh schema build (#2716)

--- frontend/src/generated/api.d.ts
+++ frontend/src/generated/api.d.ts
@@ -52193,7 +52193,9 @@ export interface operations {
   };
   magic_consequence_pool_catalog_list: {
     parameters: {
-      query?: never;
+      query?: {
+        action_category?: string;
+      };
       header?: never;
       path?: never;
       cookie?: never;
@@ -52215,7 +52217,8 @@ export interface operations {
       query?: never;
       header?: never;
       path: {
-        id: string;
+        /** @description A unique integer value identifying this Consequence Pool. */
+        id: number;
       };
       cookie?: never;
     };
