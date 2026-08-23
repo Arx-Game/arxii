adfc348e756b1a1013ce941d38453f7b8258c746
Arx Developer <surly.mime@gmail.com>
2026-08-23 01:45:55 +0000

    Give the vote-button test fixture the posthumous summary fields

--- frontend/src/journals/__tests__/JournalsPage.voteButton.test.tsx
+++ frontend/src/journals/__tests__/JournalsPage.voteButton.test.tsx
@@ -84,6 +84,9 @@ function makeEntry(overrides: Partial<JournalEntrySummary> = {}): JournalEntrySu
     edited_at: null,
     tags: [],
     response_count: 0,
+    posthumous_override: 'inherit',
+    revealed_at: null,
+    is_posthumous: false,
     ...overrides,
   };
 }
